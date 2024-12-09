05f37702e91650ad729d767cff802da9c16d67c9
wen2720 <wenhao2720@gmail.com>
2024-12-09 18:41:57 +0100

    test

--- pyPaiNN.py
+++ pyPaiNN.py
@@ -462,7 +462,7 @@ smoothing_factor = 0.9
 wait = 0
 
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-    optimizer, mode="min", factor=0.5, patience=5, threshold=1e-4
+    optimizer, mode="min", factor=0.5, patience=5, threshold=1e-8
 )
 
 painn.train()
